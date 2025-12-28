84a605956c593efd0811caa909af2230ee419ba0
ERRORawa <95691967+ERRORawa@users.noreply.github.com>
2025-12-28 17:12:11 +0800

    网页壁纸：文件选择器 [添加]file属性支持选择视频/音频类文件 [移除]一个没啥用的变量

--- Open Wallpaper Engine/ContentView/Components/Alerts/FilePicker.swift
+++ Open Wallpaper Engine/ContentView/Components/Alerts/FilePicker.swift
@@ -6,23 +6,31 @@ struct ImageFilePicker {
     @Binding var isPresented: Bool
     @Binding var isCancle: Bool
     var key: String
-    
-    private let imageTypes: [UTType] = [.image]
+    var fileType: String
     
     func showPanel() {
         let panel = NSOpenPanel()
         panel.allowsMultipleSelection = false
         panel.canChooseDirectories = false
         panel.canCreateDirectories = false
-        panel.allowedContentTypes = imageTypes
         let result = panel.runModal()
         if result == .OK, let url = panel.urls.first {
-            if url.isImageFile {
-                fileValues[key] = url
+            if fileType == "video" {
+                if url.isVideoFile {
+                    fileValues[key] = url
+                } else {
+                    isCancle = true
+                    NSAlert.showError("请选择允许的视频/音频文件\n(webm, ogg, ogv)")
+                }
             } else {
-                isCancle = true
-                NSAlert.showError()
+                if url.isImageFile {
+                    fileValues[key] = url
+                } else {
+                    isCancle = true
+                    NSAlert.showError("请选择允许的图片文件\n(jpg, jpeg, jfif, png, pnga, bmp, gif, svg, webp)")
+                }
             }
+            
         } else {
             isCancle = true
         }
@@ -36,14 +44,18 @@ extension URL {
         let imageExtensions = ["jpg", "jpeg", "jfif", "png", "pnga", "bmp", "gif", "svg", "webp"]
         return imageExtensions.contains(pathExtension.lowercased())
     }
+    var isVideoFile: Bool {
+        let videoExtensions = ["webm", "ogg", "ogv"]
+        return videoExtensions.contains(pathExtension.lowercased())
+    }
 }
 
 extension NSAlert {
     // 错误弹窗
-    static func showError() {
+    static func showError(_ message: String) {
         let alert = NSAlert()
         alert.messageText = "提示"
-        alert.informativeText = "请选择图片文件"
+        alert.informativeText = message
         alert.alertStyle = .warning
         alert.addButton(withTitle: "确定")
         alert.runModal()
